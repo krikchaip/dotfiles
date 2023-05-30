1a12756996a40482c2ab15c692eb23e8965e0079
krikchaip <krikchai.p@gmail.com>
2023-05-30 15:17:59 +0700

    refactor .zshrc

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,43 @@
+#** Set personal aliases, overriding those provided by oh-my-zsh libs,
+#** plugins, and themes. Aliases can be placed here, though oh-my-zsh
+#** users are encouraged to define aliases within the ZSH_CUSTOM folder.
+#** For a full list of active aliases, run `alias`.
+
+#** oh my zsh
+alias zshconfig="chezmoi edit --apply ~/.zshrc"
+
+#** docker client
+alias da="docker attach"
+alias db="docker build . --no-cache --progress plain -t \$(basename \$PWD)"
+alias de="docker exec -it"
+alias di="docker images"
+alias dn="docker network"
+alias dpa="docker ps -a"
+alias dri="docker rmi"
+alias drit="docker run -it --rm"
+alias drm="docker rm"
+alias drma="docker rm -fv \$(docker ps -qa)"
+alias dv="docker volume"
+alias dvl="docker volume ls"
+alias dvp="docker volume prune"
+
+#** docker compose
+alias dc="docker compose"
+alias dcd="docker compose down"
+alias dcpa="docker compose ps -a"
+alias dcu="docker compose up"
+
+#** git
+alias gfp="git fetch --prune"
+alias gwt="git worktree"
+
+#** yarn
+alias yd="yarn dev"
+
+#** pnpm
+alias pe="pnpm exec"
+alias pn="pnpm"
+alias px="pnpx"
+
+#** chezmoi
+alias cz="chezmoi"
---
+++ dot_oh-my-zsh/custom/functions.zsh
@@ -0,0 +1,49 @@
+REVIEW_DIR="$HOME/.code-review"
+
+#** create a new bash script
+create-script() {
+  local filename="$1"
+  echo '#\!/bin/bash' | tr -d '\' >$filename
+  chmod +x $filename
+  code $filename
+}
+
+#** uses when "Activity Monitor" acting weird
+restart-activity-monitor() {
+  rm ~/Library/Preferences/com.apple.ActivityMonitor.plist
+}
+
+#** restart touchbar processes
+restart-touchbar() {
+  sudo pkill TouchBarServer
+  sudo killall ControlStrip
+}
+
+#** continue on code reviewing
+review() {
+  local branch="$1"
+  if [ -d $(readlink -f "$REVIEW_DIR/$branch")]; then
+    code -n "$REVIEW_DIR/$branch"
+  fi
+}
+
+#** finish code reviewing and remove branch
+review-done() {
+  local branch="$1"
+  git worktree remove "$REVIEW_DIR/$branch"
+  git branch -d "$branch"
+}
+
+#** start code reviewing
+review-start() {
+  local branch="$1"
+  git worktree add "$REVIEW_DIR/$branch" "$branch"
+  code -n "$REVIEW_DIR/$branch"
+}
+
+#** show git commits from 10am till now
+what-have-i-done-today() {
+  git log --stat \
+    --relative-date --since="10am" \
+    --author="$(git config --get user.name)"
+}
