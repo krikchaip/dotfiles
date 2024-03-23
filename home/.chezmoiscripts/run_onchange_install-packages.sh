8608d3d0fe6d2662398c8b05525c88f5601b8c72
krikchaip <krikchai.p@gmail.com>
2024-03-23 14:31:05 +0700

    reworked on the package installation

--- home/.chezmoiscripts/run_onchange_install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# install homebrew
-if [ ! "$(command -v brew)" ]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# brew install nushell
-# brew install kitty
--- to_be_removed/dot_oh-my-zsh/custom/functions.zsh
+++ to_be_removed/dot_oh-my-zsh/custom/functions.zsh
@@ -1,13 +1,3 @@
-REVIEW_DIR="$HOME/.code-review"
-
-#** create a new bash script
-create-script() {
-  local filename="$1"
-  echo '#\!/bin/bash' | tr -d '\' >$filename
-  chmod +x $filename
-  code $filename
-}
-
 #** uses when "Activity Monitor" acting weird
 restart-activity-monitor() {
   rm ~/Library/Preferences/com.apple.ActivityMonitor.plist
@@ -19,26 +9,10 @@ restart-touchbar() {
   sudo killall ControlStrip
 }
 
-#** continue on code reviewing
-review() {
-  local branch="$1"
-  if [ -d $(readlink -f "$REVIEW_DIR/$branch") ]; then
-    code -n "$REVIEW_DIR/$branch"
-  fi
-}
-
-#** finish code reviewing and remove branch
-review-done() {
-  local branch="$1"
-  git worktree remove "$REVIEW_DIR/$branch"
-  git branch -D "$branch"
-}
-
-#** start code reviewing
-review-start() {
-  local branch="$1"
-  git worktree add "$REVIEW_DIR/$branch" "$branch"
-  code -n "$REVIEW_DIR/$branch"
+#** apply system settings immediately without restarting computer
+apply-settings() {
+  osascript -e 'tell application "System Preferences" to quit'
+  /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 }
 
 #** show git commits from 10am till now
@@ -46,10 +20,4 @@ what-have-i-done-today() {
   git log --stat \
     --relative-date --since="10am" \
     --author="$(git config --get user.name)"
-}
-
-#** apply system settings immediately without restarting computer
-apply-settings() {
-  osascript -e 'tell application "System Preferences" to quit'
-  /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 }
