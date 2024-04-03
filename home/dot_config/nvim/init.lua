c43c30a8babbc362b35017c8e0c75c971f522ff5
krikchaip <krikchai.p@gmail.com>
2024-04-03 19:42:55 +0700

    nvim: telescope add icons

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -42,6 +42,7 @@ local plugins = {
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
