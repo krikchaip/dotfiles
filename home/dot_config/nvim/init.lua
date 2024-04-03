fce686a6aca942e80d9a675a2397a4b4e670041f
krikchaip <krikchai.p@gmail.com>
2024-04-03 16:20:58 +0700

    nvim refactor

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,22 +1,22 @@
-vim.cmd("set expandtab")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set tabstop=2")
-vim.cmd("set shell=nu")
+vim.cmd('set expandtab')
+vim.cmd('set shiftwidth=2')
+vim.cmd('set softtabstop=2')
+vim.cmd('set tabstop=2')
+vim.cmd('set shell=nu')
 
-vim.g.mapleader = "\\"
+vim.g.mapleader = '\\'
 
 vim.keymap.set('n', '<leader>bd', ':bdelete<Cr>')
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -25,13 +25,13 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
   {
-    "loctvl842/monokai-pro.nvim",
+    'loctvl842/monokai-pro.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      require("monokai-pro").setup({
+      require('monokai-pro').setup({
         transparent_background = true,
-        inc_search = "underline"
+        inc_search = 'underline'
       })
 
       vim.cmd([[colorscheme monokai-pro]])
@@ -56,9 +56,9 @@ local plugins = {
       vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>bl', builtin.buffers, {})
     end
-  }
+  },
 }
 
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require('lazy').setup(plugins, opts)
