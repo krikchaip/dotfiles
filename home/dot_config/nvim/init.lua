4c88544975f144d80810eac5edd54d5429101e49
krikchaip <krikchai.p@gmail.com>
2024-04-09 14:17:00 +0700

    nvim: kickstart.nvim start over

---
+++ home/dot_config/nvim/init.lua
@@ -0,0 +1,15 @@
+--[[
+  If you don't know anything about Lua, I recommend taking some time to read through
+  a guide. One possible example which will only take 10-15 minutes:
+    - https://learnxinyminutes.com/docs/lua
+
+  After understanding a bit more about Lua, you can use `:help lua-guide` as a
+  reference for how Neovim integrates Lua.
+    - :help lua-guide
+    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+
+  If you experience any errors while trying to install plugins,
+  run `:checkhealth` for more info. for example `:checkhealth telescope`
+--]]
+
+require 'core'
