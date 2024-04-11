8cdaaac9ad223609024258537f95335751db595b
krikchaip <krikchai.p@gmail.com>
2024-04-11 15:54:26 +0700

    search -> finder

--- home/dot_config/nvim/lua/lazy-nvim/plugins/finder.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/finder.lua
@@ -51,9 +51,10 @@ return {
       },
     },
     config = function()
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
+      local builtin = require 'telescope.builtin' -- See `:help telescope.builtin`
+      local actions = require 'telescope.actions' -- See `:help telescope.actions`
 
+      -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
         -- defaults = {
         --   mappings = {
@@ -74,15 +75,11 @@ return {
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
 
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
 
-      -- See `:help telescope.actions`
-      local actions = require("telescope.actions")
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 
-      -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       -- vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       -- vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
