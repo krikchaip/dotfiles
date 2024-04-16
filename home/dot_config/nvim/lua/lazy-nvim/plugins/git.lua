4ae78dbc99127cab6923c9e413ceddf8a798f8d3
krikchaip <krikchai.p@gmail.com>
2024-04-17 03:16:16 +0700

    some gitsigns mod

--- home/dot_config/nvim/lua/lazy-nvim/plugins/git.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/git.lua
@@ -8,17 +8,17 @@ return {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs                             = {
-        add          = { text = '+' },
-        change       = { text = '~' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
+        -- add          = { text = '+' },
+        -- change       = { text = '~' },
+        -- delete       = { text = '_' },
+        -- topdelete    = { text = '‾' },
+        -- changedelete = { text = '~' },
       },
 
       attach_to_untracked               = true,
 
       signcolumn                        = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl                             = false, -- Toggle with `:Gitsigns toggle_numhl`
+      numhl                             = true,  -- Toggle with `:Gitsigns toggle_numhl`
       linehl                            = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff                         = false, -- Toggle with `:Gitsigns toggle_word_diff`
       current_line_blame                = true,  -- Toggle with `:Gitsigns toggle_current_line_blame`
