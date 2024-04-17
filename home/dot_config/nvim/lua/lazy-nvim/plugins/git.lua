29d20b82816cb838a1cda469cd971c66e192cb20
krikchaip <krikchai.p@gmail.com>
2024-04-18 01:23:19 +0700

    fix gitsigns version

--- home/dot_config/nvim/lua/lazy-nvim/plugins/git.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/git.lua
@@ -6,6 +6,7 @@ return {
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    version = '^0.8.0',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects'
     },
