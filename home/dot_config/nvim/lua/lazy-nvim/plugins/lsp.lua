824cf6a8f75d4e277a1b1bef196a518a2e2fd0bf
krikchaip <krikchai.p@gmail.com>
2024-04-19 03:02:51 +0700

    add neodev support

--- home/dot_config/nvim/lua/lazy-nvim/plugins/lsp.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/lsp.lua
@@ -1,9 +1,10 @@
 return {
-  { -- a package manager for LSP servers, DAP servers linters and formatters
-    -- ref: https://github.com/williamboman/mason.nvim
-    -- NOTE: is optimized to load as little as possible during setup.
-    --       Lazy-loading the plugin, or somehow deferring the setup,
-    --       is not recommended.
+  -- a package manager for LSP servers, DAP servers linters and formatters
+  -- ref: https://github.com/williamboman/mason.nvim
+  -- NOTE: is optimized to load as little as possible during setup.
+  --       Lazy-loading the plugin, or somehow deferring the setup,
+  --       is not recommended.
+  {
     'williamboman/mason.nvim',
     name = 'mason',
     opts = {
@@ -25,8 +26,9 @@ return {
     end
   },
 
-  { -- closes gaps that exist between mason.nvim and nvim-lspconfig
-    -- ref: https://github.com/williamboman/mason-lspconfig.nvim
+  -- closes gaps that exist between mason.nvim and nvim-lspconfig
+  -- ref: https://github.com/williamboman/mason-lspconfig.nvim
+  {
     'williamboman/mason-lspconfig.nvim',
     name = 'mason-lspconfig',
     dependencies = { 'mason' },
@@ -47,11 +49,29 @@ return {
     end
   },
 
-  { -- actual configs and apis for the Nvim LSP client
-    -- ref: https://github.com/neovim/nvim-lspconfig
+  -- manages Mason package installation and updates
+  -- ref: https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+  -- {
+  --   'WhoIsSethDaniel/mason-tool-installer.nvim',
+  --   name = 'mason-tool-installer',
+  --   dependencies = { 'mason', 'mason-lspconfig' },
+  --   opts = {},
+  -- },
+
+  -- configures lua_ls for completion, annotations and signatures of Neovim apis
+  -- ref: https://github.com/folke/neodev.nvim
+  {
+    'folke/neodev.nvim',
+    name = 'neodev',
+    opts = {},
+  },
+
+  -- actual configs and apis for the Nvim LSP client
+  -- ref: https://github.com/neovim/nvim-lspconfig
+  {
     'neovim/nvim-lspconfig',
     name = 'lspconfig',
-    dependencies = { 'mason', 'mason-lspconfig' },
+    dependencies = { 'mason', 'mason-lspconfig', 'neodev' },
     config = function()
       local lspconfig = require 'lspconfig'
       -- local mason_lspconfig = require 'mason-lspconfig'
@@ -62,4 +82,8 @@ return {
       lspconfig.lua_ls.setup {}
     end
   },
+
+  -- Status updates UI for LSP.
+  -- ref: https://github.com/j-hui/fidget.nvim
+  -- { 'j-hui/fidget.nvim' },
 }
