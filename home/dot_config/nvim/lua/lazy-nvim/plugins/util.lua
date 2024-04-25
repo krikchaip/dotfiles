8f7fc7f959db10f3932c45aa271c7169d744f30f
krikchaip <krikchai.p@gmail.com>
2024-04-25 20:13:04 +0700

    add lua async util

--- home/dot_config/nvim/lua/lazy-nvim/plugins/util.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/util.lua
@@ -2,13 +2,19 @@ return {
   -- A collection of Nvim utility functions
   {
     'nvim-lua/plenary.nvim',
-    name = 'plenary'
+    name = 'plenary',
   },
 
-  -- Useful for getting pretty icons, but requires a Nerd Font to be installed.
+  -- Useful for getting pretty icons, but requires a Nerd Font to be installed
   {
     'nvim-tree/nvim-web-devicons',
     name = 'web-devicons',
-    enabled = vim.g.have_nerd_font
+    enabled = vim.g.have_nerd_font,
   },
+
+  -- A Lua version of Javascript's Promise & Async-Await
+  {
+    'kevinhwang91/promise-async',
+    name = 'promise-async',
+  }
 }
