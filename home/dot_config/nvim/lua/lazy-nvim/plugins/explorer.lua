3aa882e1b9c135b5e6ba8d30956fd2c08b65afe6
krikchaip <krikchai.p@gmail.com>
2024-04-30 15:49:26 +0700

    basic nvim-tree setup

--- home/dot_config/nvim/lua/lazy-nvim/plugins/explorer.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/explorer.lua
@@ -1,10 +1,27 @@
 return {
   {
-    'nvim-neo-tree/neo-tree.nvim',
-    name = 'neo-tree',
-    branch = 'v3.x',
-    cmd = 'Neotree',
-    dependencies = { 'plenary', 'web-devicons', 'nui', 'image' },
-    opts = {},
-  }
+    'nvim-tree/nvim-tree.lua',
+    name = 'nvim-tree',
+    version = '*',
+    lazy = false,
+    dependencies = { 'web-devicons' },
+    opts = {
+      on_attach = function(bufnr)
+        local api = require 'nvim-tree.api'
+
+        local opts = { buffer = bufnr, silent = true, nowait = true }
+
+        opts.desc = 'Toggle Help'
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts)
+      end,
+    },
+    init = function()
+      -- disable netrw at the very start of the plugin
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      -- enable 24-bit colour
+      vim.opt.termguicolors = true
+    end,
+  },
 }
