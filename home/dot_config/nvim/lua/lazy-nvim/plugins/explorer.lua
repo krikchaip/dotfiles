0e018e628880932f988fbd09fecd6888ba9f19b6
krikchaip <krikchai.p@gmail.com>
2024-05-01 20:23:51 +0700

    keymap remap finished

--- home/dot_config/nvim/lua/lazy-nvim/plugins/explorer.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/explorer.lua
@@ -21,57 +21,6 @@ return {
           noremap = true,
         }
 
-        -- ['>'] = {  api.node.navigate.sibling.next, 'Next Sibling' },
-        -- ['<'] = {  api.node.navigate.sibling.prev, 'Previous Sibling' },
-        -- ['J'] = {  api.node.navigate.sibling.last, 'Last Sibling' },
-        -- ['K'] = {  api.node.navigate.sibling.first, 'First Sibling' },
-
-        -- ['S'] = {  api.tree.search_node, 'Search' },
-        -- ['F'] = {  api.live_filter.clear, 'Live Filter: Clear' },
-        -- ['f'] = {  api.live_filter.start, 'Live Filter: Start' },
-
-        -- ['<C-]>'] = {  api.tree.change_root_to_node, 'CD: Into Node' },
-        -- ['<C-[>'] = {  api.tree.change_root_to_parent, 'CD: Up' },
-
-        -- ['bd'] = {  api.marks.bulk.delete, 'Delete Bookmarked' },
-        -- ['bt'] = {  api.marks.bulk.trash, 'Trash Bookmarked' },
-        -- ['bmv'] = {  api.marks.bulk.move, 'Move Bookmarked' },
-        -- ['m'] = {  api.marks.toggle, 'Toggle Bookmark' },
-
-        -- ['[c'] = {  api.node.navigate.git.prev, 'Prev Git' },
-        -- [']c'] = {  api.node.navigate.git.next, 'Next Git' },
-        -- [']e'] = {  api.node.navigate.diagnostics.next, 'Next Diagnostic' },
-        -- ['[e'] = {  api.node.navigate.diagnostics.prev, 'Prev Diagnostic' },
-
-        -- ['L'] = {  api.node.open.toggle_group_empty, 'Toggle Group Empty' },
-        -- ['B'] = {  api.tree.toggle_no_buffer_filter, 'Toggle Filter: No Buffer' },
-        -- ['C'] = {  api.tree.toggle_git_clean_filter, 'Toggle Filter: Git Clean' },
-        -- ['M'] = {  api.tree.toggle_no_bookmark_filter, 'Toggle Filter: No Bookmark' },
-        -- ['H'] = {  api.tree.toggle_hidden_filter, 'Toggle Filter: Dotfiles' },
-        -- ['I'] = {  api.tree.toggle_gitignore_filter, 'Toggle Filter: Git Ignore' },
-        -- ['U'] = {  api.tree.toggle_custom_filter, 'Toggle Filter: Hidden' },
-
-        -- ['c'] = {  api.fs.copy.node, 'Copy' },
-        -- ['y'] = {  api.fs.copy.filename, 'Copy Name' },
-        -- ['Y'] = {  api.fs.copy.relative_path, 'Copy Relative Path' },
-        -- ['gy'] = {  api.fs.copy.absolute_path, 'Copy Absolute Path' },
-        -- ['ge'] = {  api.fs.copy.basename, 'Copy Basename' },
-
-        -- ['<C-r>'] = {  api.fs.rename_sub, 'Rename: Omit Filename' },
-        -- ['e'] = {  api.fs.rename_basename, 'Rename: Basename' },
-        -- ['r'] = {  api.fs.rename, 'Rename' },
-        -- ['u'] = {  api.fs.rename_full, 'Rename: Full Path' },
-
-        -- ['i'] = {  api.node.show_info_popup, 'Info' },
-        -- ['a'] = {  api.fs.create, 'Create File Or Directory' },
-        -- ['d'] = {  api.fs.remove, 'Delete' },
-        -- ['D'] = {  api.fs.trash, 'Trash' },
-        -- ['p'] = {  api.fs.paste, 'Paste' },
-        -- ['x'] = {  api.fs.cut, 'Cut' },
-
-        -- ['.'] = {  api.node.run.cmd, 'Run Command' },
-        -- ['s'] = {  api.node.run.system, 'Run System' },
-
         local mappings = {
           ['Explorer'] = {
             ['?'] = { api.tree.toggle_help, 'Help' },
@@ -85,6 +34,7 @@ return {
             ['o'] = { api.node.open.edit, 'Edit' },
             ['O'] = { api.node.open.no_window_picker, 'No Window Picker' },
             ['<Tab>'] = { api.node.open.preview, 'Preview' },
+            ['s'] = { api.node.run.system, 'In System' },
           },
 
           ['Split'] = {
@@ -98,6 +48,70 @@ return {
             ['P'] = { api.node.navigate.parent, 'Goto Parent' },
             ['W'] = { api.tree.collapse_all, 'Collapse All' },
             ['E'] = { api.tree.expand_all, 'Expand All' },
+            ['<C-]>'] = { api.tree.change_root_to_node, 'CD' },
+            ['<C-[>'] = { api.tree.change_root_to_parent, 'CD ..' },
+          },
+
+          ['Navigation'] = {
+            ['<'] = { api.node.navigate.sibling.prev, 'Previous Sibling' },
+            ['>'] = { api.node.navigate.sibling.next, 'Next Sibling' },
+
+            ['J'] = { api.node.navigate.sibling.last, 'Last Sibling' },
+            ['K'] = { api.node.navigate.sibling.first, 'First Sibling' },
+
+            ['[c'] = { api.node.navigate.git.prev, 'Prev Git' },
+            [']c'] = { api.node.navigate.git.next, 'Next Git' },
+
+            ['[e'] = { api.node.navigate.diagnostics.prev, 'Prev Diagnostic' },
+            [']e'] = { api.node.navigate.diagnostics.next, 'Next Diagnostic' },
+          },
+
+          ['Copy'] = {
+            ['c'] = { api.fs.copy.node, 'Current Node' },
+            ['y'] = { api.fs.copy.filename, 'Filename' },
+            ['Y'] = { api.fs.copy.relative_path, 'Relative Path' },
+            ['gy'] = { api.fs.copy.absolute_path, 'Absolute Path' },
+            ['ge'] = { api.fs.copy.basename, 'Basename' },
+          },
+
+          ['Rename'] = {
+            ['<C-r>'] = { api.fs.rename_sub, 'Omit Filename' },
+            ['r'] = { api.fs.rename, 'Filename' },
+            ['e'] = { api.fs.rename_basename, 'Basename' },
+            ['u'] = { api.fs.rename_full, 'Full Path' },
+          },
+
+          ['Operation'] = {
+            ['i'] = { api.node.show_info_popup, 'Info' },
+            ['a'] = { api.fs.create, 'Add' },
+            ['d'] = { api.fs.remove, 'Delete' },
+            ['D'] = { api.fs.trash, 'Trash' },
+            ['p'] = { api.fs.paste, 'Paste' },
+            ['x'] = { api.fs.cut, 'Cut' },
+            ['.'] = { api.node.run.cmd, 'Run Command' },
+          },
+
+          ['Search'] = {
+            ['S'] = { api.tree.search_node, 'Exact' },
+            ['f'] = { api.live_filter.start, 'Start Filter' },
+            ['F'] = { api.live_filter.clear, 'Clear Filter' },
+          },
+
+          ['Toggle'] = {
+            ['L'] = { api.node.open.toggle_group_empty, 'Group Empty' },
+            ['M'] = { api.tree.toggle_no_bookmark_filter, 'Marks Filter' },
+            ['B'] = { api.tree.toggle_no_buffer_filter, 'Buffer Filter' },
+            ['C'] = { api.tree.toggle_git_clean_filter, 'Git Clean Filter' },
+            ['I'] = { api.tree.toggle_gitignore_filter, 'Git Ignore Filter' },
+            ['H'] = { api.tree.toggle_hidden_filter, 'Dotfiles Filter' },
+            ['U'] = { api.tree.toggle_custom_filter, 'Hidden Filter' },
+          },
+
+          ['Marks'] = {
+            ['m'] = { api.marks.toggle, 'Toggle Current' },
+            ['bd'] = { api.marks.bulk.delete, 'Delete Selected' },
+            ['bt'] = { api.marks.bulk.trash, 'Trash Selected' },
+            ['bmv'] = { api.marks.bulk.move, 'Move Selected' },
           },
         }
 
