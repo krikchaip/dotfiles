6f95f8b46085896b17dfeeef87a152846a7d29d8
krikchaip <krikchai.p@gmail.com>
2024-05-01 14:47:24 +0700

    disable netrw

--- home/dot_config/nvim/lua/lazy-nvim/plugins/explorer.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/explorer.lua
@@ -6,6 +6,9 @@ return {
     lazy = false,
     dependencies = { 'web-devicons' },
     opts = {
+      disable_netrw = true,
+      hijack_netrw = true,
+
       on_attach = function(bufnr)
         -- local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
 
@@ -16,12 +19,11 @@ return {
         end
 
         vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
-        vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer, opts('Open: In Place'))
         vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
         vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
         vim.keymap.set('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
         vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
-        vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+        vim.keymap.set('n', '<C-s>', api.node.open.horizontal, opts('Open: Horizontal Split'))
         vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
         vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
         vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
@@ -72,7 +74,6 @@ return {
         vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
         vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
         vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
-        vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
       end,
     },
     init = function()
