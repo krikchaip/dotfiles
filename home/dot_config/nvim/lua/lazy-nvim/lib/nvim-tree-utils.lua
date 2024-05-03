66a99aaffff404de77ddd3b976321464d8c1a58c
krikchaip <krikchai.p@gmail.com>
2024-05-04 03:45:33 +0700

    nvim-tree: 2 more utils

--- home/dot_config/nvim/lua/lazy-nvim/lib/nvim-tree-utils.lua
+++ home/dot_config/nvim/lua/lazy-nvim/lib/nvim-tree-utils.lua
@@ -94,4 +94,45 @@ function M.change_root_to_global_cwd()
   api.tree.change_root(global_cwd)
 end
 
+-- Sometimes, we only want to open a tab,
+-- but don't want to jump to that tab immediately
+function M.open_tab_silent(node)
+  local api = require 'nvim-tree.api'
+
+  api.node.open.tab(node)
+  vim.cmd.tabprev()
+end
+
+-- Sorting files naturally (respecting numbers within files names)
+-- ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#sorting-files-naturally-respecting-numbers-within-files-names
+function M.sort_by_natural_cmp(nodes)
+  local sorter = function(left, right)
+    left = left.name:lower()
+    right = right.name:lower()
+
+    if left == right then
+      return false
+    end
+
+    for i = 1, math.max(string.len(left), string.len(right)), 1 do
+      local l = string.sub(left, i, -1)
+      local r = string.sub(right, i, -1)
+
+      if type(tonumber(string.sub(l, 1, 1))) == "number" and
+          type(tonumber(string.sub(r, 1, 1))) == "number" then
+        local l_number = tonumber(string.match(l, "^[0-9]+"))
+        local r_number = tonumber(string.match(r, "^[0-9]+"))
+
+        if l_number ~= r_number then
+          return l_number < r_number
+        end
+      elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
+        return l < r
+      end
+    end
+  end
+
+  table.sort(nodes, sorter)
+end
+
 return M
