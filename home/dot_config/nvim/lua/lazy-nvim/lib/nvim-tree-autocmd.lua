b0fd7fe25116e3dbe12b31922e0d72a98b9ca643
krikchaip <krikchai.p@gmail.com>
2024-05-04 16:48:51 +0700

    comment out nvim-tree-bdelete for now

--- home/dot_config/nvim/lua/lazy-nvim/lib/nvim-tree-autocmd.lua
+++ home/dot_config/nvim/lua/lazy-nvim/lib/nvim-tree-autocmd.lua
@@ -18,29 +18,29 @@ vim.api.nvim_create_autocmd('BufEnter', {
 
 -- Go to last used hidden buffer after deleting a buffer
 -- ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#go-to-last-used-hidden-buffer-when-deleting-a-buffer
-vim.api.nvim_create_autocmd('BufEnter', {
-  desc = 'Prevent nvim-tree from fully expand when there some buffers in the background',
-  group = vim.api.nvim_create_augroup('nvim-tree-bdelete', { clear = true }),
-  nested = true,
-  callback = function()
-    local api = require 'nvim-tree.api'
-
-    -- Only 1 window with nvim-tree left: we probably closed a file buffer
-    if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
-      -- Required to let the close event complete. An error is thrown without this.
-      vim.defer_fn(function()
-        -- close nvim-tree: will go to the last hidden buffer used before closing
-        api.tree.toggle({ find_file = true, focus = true })
-
-        -- re-open nivm-tree
-        api.tree.toggle({ find_file = true, focus = true })
-
-        -- nvim-tree is still the active window. Go to the previous window.
-        vim.cmd('wincmd p')
-      end, 0)
-    end
-  end
-})
+-- vim.api.nvim_create_autocmd('BufEnter', {
+--   desc = 'Prevent nvim-tree from fully expand when there some buffers in the background',
+--   group = vim.api.nvim_create_augroup('nvim-tree-bdelete', { clear = true }),
+--   nested = true,
+--   callback = function()
+--     local api = require 'nvim-tree.api'
+--
+--     -- Only 1 window with nvim-tree left: we probably closed a file buffer
+--     if #vim.api.nvim_list_wins() == 1 and api.tree.is_tree_buf() then
+--       -- Required to let the close event complete. An error is thrown without this.
+--       vim.defer_fn(function()
+--         -- close nvim-tree: will go to the last hidden buffer used before closing
+--         api.tree.toggle({ find_file = true, focus = true })
+--
+--         -- re-open nivm-tree
+--         api.tree.toggle({ find_file = true, focus = true })
+--
+--         -- nvim-tree is still the active window. Go to the previous window.
+--         vim.cmd('wincmd p')
+--       end, 0)
+--     end
+--   end
+-- })
 
 -- Autoclose nvim-tree if it is the last buffer in a tab
 -- (only works with `:q`, `:qa` and `<C-W>q` or any other window cmds)
