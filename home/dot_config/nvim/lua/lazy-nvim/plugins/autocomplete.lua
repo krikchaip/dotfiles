13436f4b0fba9b9759796bdb0b9be991ee411421
krikchaip <krikchai.p@gmail.com>
2024-05-11 15:27:00 +0700

    autocomplete: luasnip integration

---
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/autocomplete.lua
@@ -0,0 +1,59 @@
+return {
+  -- A Collection of VSCode snippets across many different programming languages
+  { 'rafamadriz/friendly-snippets', name = 'friendly-snippets' },
+
+  -- Snippet provider engine
+  {
+    'L3MON4D3/LuaSnip',
+    name = 'luasnip',
+    version = 'v2.*',
+    build = vim.fn.has 'win32' ~= 0 and 'make install_jsregexp' or nil,
+    dependencies = { 'friendly-snippets' },
+    opts = {},
+    config = function(_, opts)
+      require('luasnip').config.setup(opts)
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
+
+  -- Completion sources
+  { 'saadparwaiz1/cmp_luasnip', name = 'cmp.luasnip' },
+
+  {
+    'hrsh7th/nvim-cmp',
+    name = 'cmp',
+    event = { 'InsertEnter' },
+    dependencies = { 'cmp.luasnip' },
+    config = function()
+      local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
+
+      cmp.setup {
+        snippet = {
+          expand = function(args) luasnip.lsp_expand(args.body) end,
+        },
+
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+
+        mapping = cmp.mapping.preset.insert {
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+
+          -- Accept currently selected item. Set `select` to `false`
+          -- to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+        },
+
+        sources = cmp.config.sources {
+          -- { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        },
+      }
+    end,
+  },
+}
