0ede99b3b3a2af7a7b4b05ac35cc143a93fb843b
krikchaip <krikchai.p@gmail.com>
2024-05-21 16:53:46 +0700

    session: remove help and blank

--- home/dot_config/nvim/lua/lazy-nvim/plugins/editor.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/editor.lua
@@ -49,9 +49,9 @@ return {
         -- When restoring plugin help pages (eg. telescope), it also requires the plugin to be loaded first.
         -- Therefore, if the plugin is lazy loaded while having your session containing its help page.
         -- There will be times that restoring the session might fail.
-        'help',
+        -- 'help',
 
-        'blank',
+        -- 'blank',
         'buffers',
         'curdir',
         'folds',
