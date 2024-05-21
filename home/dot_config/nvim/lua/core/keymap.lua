d9bffd69625f8a8c397bd3de45af74b808cd84d1
krikchaip <krikchai.p@gmail.com>
2024-05-21 17:26:43 +0700

    keymaps: refactored

--- home/dot_config/nvim/lua/core/keymap.lua
+++ home/dot_config/nvim/lua/core/keymap.lua
@@ -6,16 +6,35 @@ vim.keymap.set({ 'i', 'c' }, '<C-u>', '<Up>')
 vim.keymap.set({ 'i', 'c' }, '<C-a>', '<Home>')
 vim.keymap.set({ 'i', 'c' }, '<C-e>', '<End>')
 
+-- Horizontal Scrolling
+vim.keymap.set('n', '<M-h>', 'zH', { desc = 'Scroll half page left' })
+vim.keymap.set('n', '<M-l>', 'zL', { desc = 'Scroll half page right' })
+vim.keymap.set('n', '<M-S-h>', 'zh', { desc = 'Scroll left' })
+vim.keymap.set('n', '<M-S-l>', 'zl', { desc = 'Scroll right' })
+
+-- Vertical Scrolling
+vim.keymap.set('n', '<M-j>', '<C-d>zz', { desc = 'Scroll half page down' })
+vim.keymap.set('n', '<M-k>', '<C-u>zz', { desc = 'Scroll half page up' })
+vim.keymap.set('n', '<M-S-j>', 'jzz', { desc = 'Scroll down' })
+vim.keymap.set('n', '<M-S-k>', 'kzz', { desc = 'Scroll up' })
+
+-- Exit terminal mode in the builtin terminal (default: <C-\><C-n>)
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Clear search highlights on pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Fix visual mode yank region cursor moving back to the top
+-- ref: https://stackoverflow.com/questions/3806629/yank-a-region-in-vim-without-the-cursor-moving-to-the-top-of-the-block
+vim.keymap.set('x', 'y', 'ygv<Esc>')
+
 -- Simple autoclose in command mode
 vim.keymap.set('c', '{', '{}<Left>')
 vim.keymap.set('c', '[', '[]<Left>')
 vim.keymap.set('c', '(', '()<Left>')
 vim.keymap.set('c', "'", "''<Left>")
 vim.keymap.set('c', '"', '""<Left>')
-
--- Fix visual mode yank region cursor moving back to the top
--- ref: https://stackoverflow.com/questions/3806629/yank-a-region-in-vim-without-the-cursor-moving-to-the-top-of-the-block
-vim.keymap.set('x', 'y', 'ygv<Esc>')
 
 -- Substitute line while on insert mode (useful for inserting indentation on an empty line)
 vim.keymap.set('i', '<C-s>', '<C-o>S')
@@ -28,22 +47,9 @@ vim.keymap.set('x', '<M-S-.>', '>gv', { desc = 'Insert one indent to this region
 vim.keymap.set('n', '<M-S-,>', '<<', { desc = 'Remove one indent from this line' })
 vim.keymap.set('n', '<M-S-.>', '>>', { desc = 'Insert one indent to this line' })
 
--- Horizontal Scrolling
-vim.keymap.set('n', '<M-h>', 'zH', { desc = 'Scroll half page left' })
-vim.keymap.set('n', '<M-l>', 'zL', { desc = 'Scroll half page right' })
-vim.keymap.set('n', '<M-S-h>', 'zh', { desc = 'Scroll left' })
-vim.keymap.set('n', '<M-S-l>', 'zl', { desc = 'Scroll right' })
-
--- Clear search highlights on pressing <Esc> in normal mode
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
 -- Make Increment/Decrement key more intuitive
 vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement number' })
 vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment number' })
-
--- Exit terminal mode in the builtin terminal (default: <C-\><C-n>)
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Saving buffers (files)
 vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = 'Write current buffer (save file)' })
@@ -59,24 +65,24 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<C-w>n', '<cmd>vnew<CR>', { desc = 'Split a new empty window vertically' })
-vim.keymap.set('n', '<C-w>N', '<cmd>new<CR>', { desc = 'Split a new empty window horizontally' })
-
 -- `<C-w>w` doesn't work when there's more than 2 windows in a tab
 -- ref: https://www.reddit.com/r/neovim/comments/pibo9c/how_to_focus_an_opened_floating_window
 vim.keymap.set('n', '<C-S-k>', '999<C-w>w', { desc = 'Move focus to the floating window' })
 
--- Tab Navigation
+-- Create a new empty window/tab
+vim.keymap.set('n', '<C-w>n', '<cmd>vnew<CR>', { desc = 'Split a new empty window vertically' })
+vim.keymap.set('n', '<C-w>N', '<cmd>new<CR>', { desc = 'Split a new empty window horizontally' })
 vim.keymap.set('n', '<C-n>', '<cmd>tabnew<CR>', { desc = 'Create a new empty tab' })
+
+-- Tabpage Manipulation
+vim.keymap.set('n', '<C-S-left>', '<cmd>-tabmove<CR>', { desc = 'Move the current tab backward' })
+vim.keymap.set('n', '<C-S-right>', '<cmd>+tabmove<CR>', { desc = 'Move the current tab forward' })
 vim.keymap.set('n', '<leader>tq', '<cmd>tabclose<CR>', { desc = 'Close the current tab' })
 vim.keymap.set('n', '<leader>to', '<cmd>tabonly<CR>', { desc = 'Close all other tab pages' })
 
-vim.keymap.set('n', '<C-S-left>', '<cmd>-tabmove<CR>', { desc = 'Move the current tab backward' })
-vim.keymap.set('n', '<C-S-right>', '<cmd>+tabmove<CR>', { desc = 'Move the current tab forward' })
-
+-- Tabpage Navigation
 vim.keymap.set('n', '<C-left>', '<cmd>tabprevious<CR>', { desc = 'Go to previous tab' })
 vim.keymap.set('n', '<C-right>', '<cmd>tabnext<CR>', { desc = 'Go to next tab' })
-
 vim.keymap.set('n', '<C-1>', '<cmd>1tabnext<CR>', { desc = 'Go to tab #1' })
 vim.keymap.set('n', '<C-2>', '<cmd>2tabnext<CR>', { desc = 'Go to tab #2' })
 vim.keymap.set('n', '<C-3>', '<cmd>3tabnext<CR>', { desc = 'Go to tab #3' })
