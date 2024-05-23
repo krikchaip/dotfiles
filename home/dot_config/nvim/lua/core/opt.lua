8699820066c24cdfb2a44dde829015e066193f87
krikchaip <krikchai.p@gmail.com>
2024-05-23 22:14:24 +0700

    opt: rearrange

--- home/dot_config/nvim/lua/core/opt.lua
+++ home/dot_config/nvim/lua/core/opt.lua
@@ -1,5 +1,3 @@
--- [[ option-list, vim.opt, vim.o, vim.g ]]
-
 -- Set <space> as the leader key
 -- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
@@ -12,14 +10,26 @@ vim.g.have_nerd_font = true
 vim.g.nu_config_path = '~/Library/Application Support/nushell/config.nu'
 vim.g.nu_env_path = '~/Library/Application Support/nushell/env.nu'
 
+-- Configure the default shell to nushell
+-- ref: https://github.com/neovim/neovim/issues/19648
+vim.opt.shell = 'nu'
+vim.opt.shelltemp = false
+vim.opt.shellcmdflag = '--stdin ' .. '--config "' .. vim.g.nu_config_path .. '" --env-config "' .. vim.g.nu_env_path .. '" -c'
+
 -- Make line numbers default
 vim.opt.number = true
 
 -- Show relative line numbers (to help with jumping)
 -- vim.opt.relativenumber = true
 
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
 -- Do not wrap lines
 vim.opt.wrap = false
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 5
 
 -- Columns to scroll horizontally when texts are off the screen
 vim.opt.sidescrolloff = 4
@@ -46,12 +56,15 @@ vim.opt.undofile = true
 -- No swapfile. EVER 😠
 vim.opt.swapfile = false
 
+-- Set highlight on search
+vim.opt.hlsearch = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
 
 -- Decrease update time (default 4000ms, floating menus popup sooner on hover)
 vim.opt.updatetime = 500
@@ -63,11 +76,14 @@ vim.opt.timeoutlen = 500
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Folds will be enabled again by plugins
-vim.opt.foldenable = false
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
 
 -- Limit fold columns to just one (chevron icon)
 vim.opt.foldcolumn = '1'
+
+-- Folds will be enabled again by plugins
+vim.opt.foldenable = false
 
 -- Open (expand) all folds by default
 -- ref: https://stackoverflow.com/questions/5784677/the-first-time-i-close-a-fold-it-closes-all-folds
@@ -80,21 +96,3 @@ vim.opt.foldopen:remove 'hor'
 -- Sets how neovim will display certain whitespace characters in the editor.
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 5
-
--- Set highlight on search
-vim.opt.hlsearch = true
-
--- Configure the default shell to nushell
--- ref: https://github.com/neovim/neovim/issues/19648
-vim.opt.shell = 'nu'
-vim.opt.shelltemp = false
-vim.opt.shellcmdflag = '--stdin ' .. '--config "' .. vim.g.nu_config_path .. '" --env-config "' .. vim.g.nu_env_path .. '" -c'
