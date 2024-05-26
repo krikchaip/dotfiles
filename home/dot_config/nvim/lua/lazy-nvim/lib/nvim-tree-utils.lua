8853203d9ba693c717d4fc2398abbfc65c89a767
krikchaip <krikchai.p@gmail.com>
2024-05-27 04:15:29 +0700

    nvim-tree-utils: formatting

--- home/dot_config/nvim/lua/lazy-nvim/lib/nvim-tree-utils.lua
+++ home/dot_config/nvim/lua/lazy-nvim/lib/nvim-tree-utils.lua
@@ -11,9 +11,7 @@ local function view_selection(prompt_bufnr, _)
     local selection = action_state.get_selected_entry()
     local filename = selection.filename
 
-    if (filename == nil) then
-      filename = selection[1]
-    end
+    if filename == nil then filename = selection[1] end
 
     openfile.fn('', filename)
   end)
@@ -25,17 +23,13 @@ local function launch_telescope(func_name, opts)
   local api = require 'nvim-tree.api'
   local telescope_status_ok, _ = pcall(require, 'telescope')
 
-  if not telescope_status_ok then
-    return
-  end
+  if not telescope_status_ok then return end
 
   local node = api.tree.get_node_under_cursor()
   local is_folder = node.fs_stat and node.fs_stat.type == 'directory' or false
   local basedir = is_folder and node.absolute_path or vim.fn.fnamemodify(node.absolute_path, ':h')
 
-  if (node.name == '..' and TreeExplorer ~= nil) then
-    basedir = TreeExplorer.cwd
-  end
+  if node.name == '..' and TreeExplorer ~= nil then basedir = TreeExplorer.cwd end
 
   opts = opts or {}
   opts.cwd = basedir
@@ -47,9 +41,7 @@ end
 
 local M = {}
 
-function M.collapse_all()
-  require('nvim-tree.api').tree.collapse_all(true)
-end
+function M.collapse_all() require('nvim-tree.api').tree.collapse_all(true) end
 
 function M.clear_all()
   require('nvim-tree.api').marks.clear()
@@ -66,7 +58,7 @@ function M.git_add_toggle()
   -- If the current node is a directory get children status
   if gs == nil then
     gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1])
-        or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
+      or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
   end
 
   -- If the file is untracked, unstaged or partially staged, we stage it
@@ -79,13 +71,9 @@ function M.git_add_toggle()
   api.tree.reload()
 end
 
-function M.launch_live_grep(opts)
-  return launch_telescope('live_grep', opts)
-end
+function M.launch_live_grep(opts) return launch_telescope('live_grep', opts) end
 
-function M.launch_find_files(opts)
-  return launch_telescope('find_files', opts)
-end
+function M.launch_find_files(opts) return launch_telescope('find_files', opts) end
 
 function M.change_root_to_global_cwd()
   local api = require 'nvim-tree.api'
@@ -110,22 +98,17 @@ function M.sort_by_natural_cmp(nodes)
     left = left.name:lower()
     right = right.name:lower()
 
-    if left == right then
-      return false
-    end
+    if left == right then return false end
 
     for i = 1, math.max(string.len(left), string.len(right)), 1 do
       local l = string.sub(left, i, -1)
       local r = string.sub(right, i, -1)
 
-      if type(tonumber(string.sub(l, 1, 1))) == 'number' and
-          type(tonumber(string.sub(r, 1, 1))) == 'number' then
+      if type(tonumber(string.sub(l, 1, 1))) == 'number' and type(tonumber(string.sub(r, 1, 1))) == 'number' then
         local l_number = tonumber(string.match(l, '^[0-9]+'))
         local r_number = tonumber(string.match(r, '^[0-9]+'))
 
-        if l_number ~= r_number then
-          return l_number < r_number
-        end
+        if l_number ~= r_number then return l_number < r_number end
       elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
         return l < r
       end
@@ -136,14 +119,14 @@ function M.sort_by_natural_cmp(nodes)
 end
 
 function M.toggle_copy_single()
-  local fs = require 'nvim-tree.api'.fs
+  local fs = require('nvim-tree.api').fs
 
   fs.clear_clipboard()
   fs.copy.node()
 end
 
 function M.toggle_cut_single()
-  local fs = require 'nvim-tree.api'.fs
+  local fs = require('nvim-tree.api').fs
 
   fs.clear_clipboard()
   fs.cut()
@@ -160,9 +143,7 @@ function M.restore_nvim_tree()
         local api = require 'nvim-tree.api'
         local view = require 'nvim-tree.view'
 
-        if not view.is_visible() then
-          api.tree.open()
-        end
+        if not view.is_visible() then api.tree.open() end
       end
     end
   end
