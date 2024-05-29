89ed64d1da7face0f5607275a4108eb464fdb969
krikchaip <krikchai.p@gmail.com>
2024-05-29 22:25:02 +0700

    git: formatting

--- home/dot_config/nvim/lua/lazy-nvim/plugins/git.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/git.lua
@@ -5,41 +5,39 @@ return {
     'lewis6991/gitsigns.nvim',
     name = 'gitsigns',
     version = '^0.8.0',
-    cond = function()
-      return is_git_repo() or is_git_file()
-    end,
+    cond = function() return is_git_repo() or is_git_file() end,
     event = { 'BufReadPost', 'BufNewFile' },
     opts = {
-      signs                             = {
-        add          = { text = '┃' },
-        change       = { text = '┃' },
-        delete       = { text = '⏵' },
-        topdelete    = { text = '⏵' },
+      signs = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '⏵' },
+        topdelete = { text = '⏵' },
         changedelete = { text = '~' },
-        untracked    = { text = '┆' },
+        untracked = { text = '┆' },
       },
 
-      attach_to_untracked               = true,
+      attach_to_untracked = true,
 
-      signcolumn                        = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl                             = true,  -- Toggle with `:Gitsigns toggle_numhl`
-      linehl                            = false, -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff                         = false, -- Toggle with `:Gitsigns toggle_word_diff`
-      current_line_blame                = true,  -- Toggle with `:Gitsigns toggle_current_line_blame`
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 
-      current_line_blame_opts           = {
-        virt_text_pos      = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      current_line_blame_opts = {
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
         virt_text_priority = 1000,
-        delay              = 500,
-        ignore_whitespace  = true,
+        delay = 500,
+        ignore_whitespace = true,
       },
 
-      current_line_blame_formatter      = '     <author>, <author_time:%R> · <summary>',
+      current_line_blame_formatter = '     <author>, <author_time:%R> · <summary>',
       current_line_blame_formatter_opts = {
         relative_time = true,
       },
 
-      on_attach                         = function(bufnr)
+      on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
         local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
 
@@ -48,22 +46,19 @@ return {
 
         -- [[ Navigation ]]
 
-        local next_hunk, prev_hunk = ts_repeat_move.make_repeatable_move_pair(
-          function()
-            if vim.wo.diff then
-              vim.cmd.normal({ ']c', bang = true })
-            else
-              gitsigns.nav_hunk('next', { preview = false })
-            end
-          end,
-          function()
-            if vim.wo.diff then
-              vim.cmd.normal({ '[c', bang = true })
-            else
-              gitsigns.nav_hunk('prev', { preview = false })
-            end
+        local next_hunk, prev_hunk = ts_repeat_move.make_repeatable_move_pair(function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk('next', { preview = false })
           end
-        )
+        end, function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk('prev', { preview = false })
+          end
+        end)
 
         kopts.desc = 'Next unstaged [c]hange'
         vim.keymap.set('n', ']c', next_hunk, kopts)
@@ -77,7 +72,7 @@ return {
         vim.keymap.set('n', '<leader>gd', function() gitsigns.diffthis() end, kopts)
 
         kopts.desc = 'Show [D]iff against last commit'
-        vim.keymap.set('n', '<leader>gD', function() gitsigns.diffthis('~') end, kopts)
+        vim.keymap.set('n', '<leader>gD', function() gitsigns.diffthis '~' end, kopts)
 
         kopts.desc = 'Show line [i]nfo'
         vim.keymap.set('n', '<leader>gi', function() gitsigns.blame_line { full = true } end, kopts)
@@ -88,9 +83,7 @@ return {
         vim.keymap.set('n', '<leader>gcs', function() gitsigns.stage_hunk() end, kopts)
 
         kopts.desc = '[s]tage selected range'
-        vim.keymap.set('v', '<leader>gcs', function()
-          gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') }
-        end, kopts)
+        vim.keymap.set('v', '<leader>gcs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, kopts)
 
         kopts.desc = '[S]tage all changes'
         vim.keymap.set('n', '<leader>gcS', function() gitsigns.stage_buffer() end, kopts)
@@ -105,9 +98,7 @@ return {
         vim.keymap.set('n', '<leader>gcr', function() gitsigns.reset_hunk() end, kopts)
 
         kopts.desc = '[r]eset selected range to staged'
-        vim.keymap.set('v', '<leader>gcr', function()
-          gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') }
-        end, kopts)
+        vim.keymap.set('v', '<leader>gcr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, kopts)
 
         kopts.desc = '[R]eset all changes to staged'
         vim.keymap.set('n', '<leader>gcR', function() gitsigns.reset_buffer() end, kopts)
@@ -121,7 +112,7 @@ return {
 
         kopts.desc = 'the change under cursor'
         vim.keymap.set({ 'o', 'x' }, 'ac', '<cmd>Gitsigns select_hunk<CR>', kopts)
-      end
+      end,
     },
     config = function(_, opts)
       require('gitsigns').setup(opts)
@@ -129,6 +120,6 @@ return {
 
       -- must reload `statuscol` because this plugin overwrites its value
       -- vim.cmd [[ silent Lazy reload statuscol ]]
-    end
+    end,
   },
 }
