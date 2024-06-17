4307f53e77492febc065ea792fe0973d1bb3c8d9
krikchaip <krikchai.p@gmail.com>
2024-06-17 18:20:11 +0700

    editor: add new window switcher plugin

--- home/dot_config/nvim/lua/lazy-nvim/plugins/editor.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/editor.lua
@@ -235,4 +235,29 @@ return {
       },
     },
   },
+
+  {
+    'sindrets/winshift.nvim',
+    name = 'winshift',
+    lazy = false,
+    keys = {
+      { '<C-w><C-m>', '<Cmd>WinShift<CR>', desc = 'Window: Start Win-Move Mode' },
+      { '<C-w>m', '<Cmd>WinShift<CR>', desc = 'Window: Start Win-Move Mode' },
+
+      { '<C-w><C-x>', '<Cmd>WinShift swap<CR>', desc = 'Window: Swap Current With Selection' },
+      { '<C-w>x', '<Cmd>WinShift swap<CR>', desc = 'Window: Swap Current With Selection' },
+
+      { '<C-w><C-h>', '<Cmd>WinShift left<CR>', desc = 'Window: Move Current Leftward' },
+      { '<C-w>h', '<Cmd>WinShift left<CR>', desc = 'Window: Move Current Leftward' },
+
+      { '<C-w><C-j>', '<Cmd>WinShift down<CR>', desc = 'Window: Move Current Downward' },
+      { '<C-w>j', '<Cmd>WinShift down<CR>', desc = 'Window: Move Current Downward' },
+
+      { '<C-w><C-k>', '<Cmd>WinShift up<CR>', desc = 'Window: Move Current Upward' },
+      { '<C-w>k', '<Cmd>WinShift up<CR>', desc = 'Window: Move Current Upward' },
+
+      { '<C-w><C-l>', '<Cmd>WinShift right<CR>', desc = 'Window: Move Current Rightward' },
+      { '<C-w>l', '<Cmd>WinShift right<CR>', desc = 'Window: Move Current Rightward' },
+    },
+  },
 }
