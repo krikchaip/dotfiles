acf736ff47d90116630ee349121d9d65cb11b7eb
krikchaip <krikchai.p@gmail.com>
2024-06-20 20:49:12 +0700

    ui: lualine paste default configs

--- home/dot_config/nvim/lua/lazy-nvim/plugins/ui.lua
+++ home/dot_config/nvim/lua/lazy-nvim/plugins/ui.lua
@@ -400,6 +400,37 @@ return {
     priority = 900,
     lazy = false,
     dependencies = { 'web-devicons' },
-    opts = {},
+    opts = {
+      options = {
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+
+        ignore_focus = {},
+      },
+
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+
+      winbar = {},
+      inactive_winbar = {},
+
+      tabline = {},
+    },
   },
 }
