9620f924f3c993bc8a9e8d33d81de7633223c47e
krikchaip <krikchai.p@gmail.com>
2024-06-24 00:10:36 +0700

    ui: navic utils (fix dynamic highlights)

---
+++ home/dot_config/nvim/lua/lazy-nvim/lib/navic-utils.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+-- Fix background color does not inherit from lualine section when `highlight = true`
+-- ref: https://github.com/SmiteshP/nvim-navic/issues/146
+function M.adjust_dynamic_highlights()
+  return function(text)
+    local navicHls = {
+      'IconsFile',
+      'IconsModule',
+      'IconsNamespace',
+      'IconsPackage',
+      'IconsClass',
+      'IconsMethod',
+      'IconsProperty',
+      'IconsField',
+      'IconsConstructor',
+      'IconsEnum',
+      'IconsInterface',
+      'IconsFunction',
+      'IconsVariable',
+      'IconsConstant',
+      'IconsString',
+      'IconsNumber',
+      'IconsBoolean',
+      'IconsArray',
+      'IconsObject',
+      'IconsKey',
+      'IconsNull',
+      'IconsEnumMember',
+      'IconsStruct',
+      'IconsEvent',
+      'IconsOperator',
+      'IconsTypeParameter',
+      'Text',
+      'Separator',
+    }
+
+    local lualineHl = vim.api.nvim_get_hl(0, { name = 'lualine_b_inactive' })
+    local bg = lualineHl.bg and ('#%06x'):format(lualineHl.bg)
+
+    for _, hlName in ipairs(navicHls) do
+      hlName = 'Navic' .. hlName
+
+      local orgHl = hlName
+      local hl
+
+      repeat -- follow linked highlights
+        hl = vim.api.nvim_get_hl(0, { name = hlName })
+        hlName = hl.link
+      until not hl.link
+
+      vim.api.nvim_set_hl(0, orgHl, { fg = hl.fg, bg = bg })
+    end
+
+    return text
+  end
+end
+
+return M
