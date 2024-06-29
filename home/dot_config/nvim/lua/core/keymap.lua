b7284c1a2f1e1f8fd254cb071d5a218830492f83
krikchaip <krikchai.p@gmail.com>
2024-06-29 14:24:05 +0700

    ⭐

--- home/dot_config/nvim/lua/core/keymap.lua
+++ home/dot_config/nvim/lua/core/keymap.lua
@@ -60,6 +60,9 @@ vim.keymap.set('c', '(', '()<Left>')
 vim.keymap.set('c', "'", "''<Left>")
 vim.keymap.set('c', '"', '""<Left>')
 
+-- Quick access to lua commands for debugging purpose
+vim.keymap.set('n', '=', ':=', { desc = 'Print Lua Expression' })
+
 -- Substitute line while on insert mode (useful for inserting indentation on an empty line)
 vim.keymap.set('i', '<M-s>', '<C-o>S', { desc = 'Substitute line' })
 
