8e6b298f58a48e8ade83782fc66b439d9387332c
krikchaip <krikchai.p@gmail.com>
2024-07-10 16:33:26 +0700

    nwin: editor surround

--- home/dot_config/nwin/lua/plugins/editor/init.lua
+++ home/dot_config/nwin/lua/plugins/editor/init.lua
@@ -1,5 +1,6 @@
 return {
   require 'plugins.editor.autopairs',
+  require 'plugins.editor.surround',
   require 'plugins.editor.vim-bbye',
   require 'plugins.editor.vim-sleuth',
   require 'plugins.editor.window-picker',
