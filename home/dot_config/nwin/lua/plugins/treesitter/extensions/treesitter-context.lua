0edf7c1913b00e7fb53eec19f99137832e10e72f
krikchaip <krikchai.p@gmail.com>
2024-07-12 17:33:49 +0700

    nwin: treesitter-context integration

---
+++ home/dot_config/nwin/lua/plugins/treesitter/extensions/treesitter-context.lua
@@ -0,0 +1,24 @@
+-- context sticky scroll
+-- ref: https://github.com/nvim-treesitter/nvim-treesitter-context
+return {
+  -- Spec Source
+  'nvim-treesitter/nvim-treesitter-context',
+  name = 'treesitter-context',
+
+  -- Spec Setup
+  opts = {
+    enable = true,
+
+    -- How many lines the window should span. Values <= 0 mean no limit.
+    max_lines = 4,
+
+    -- Maximum number of lines to show for a single context
+    multiline_threshold = 1,
+  },
+
+  -- Spec Lazy Loading
+  event = 'User FilePost',
+  keys = {
+    { '[C', '<cmd>lua require("treesitter-context").go_to_context(vim.v.count1)<CR>', desc = 'Treesitter: Parent Context' },
+  },
+}
