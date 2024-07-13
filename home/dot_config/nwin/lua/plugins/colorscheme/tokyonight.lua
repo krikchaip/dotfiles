bee533bf88201d3611f3baae031a25f171d2c303
krikchaip <krikchai.p@gmail.com>
2024-07-14 04:20:26 +0700

    nwin: fix tokyonight plugins highlights

--- home/dot_config/nwin/lua/plugins/colorscheme/tokyonight.lua
+++ home/dot_config/nwin/lua/plugins/colorscheme/tokyonight.lua
@@ -9,10 +9,22 @@ return {
   -- Spec Setup
   config = function()
     require('tokyonight').setup {
+      -- The theme comes in four styles, `moon`, `storm`, a darker variant `night` and `day`
       style = 'storm',
+
+      -- Enable this to disable setting the background color
       transparent = false,
+
       styles = {
+        -- Background styles. Can be "dark", "transparent" or "normal"
         floats = 'transparent',
+      },
+
+      -- add any plugins here that you want to enable
+      -- for all possible plugins, see: https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+      plugins = {
+        -- enable all plugins highlights
+        all = true,
       },
     }
 
