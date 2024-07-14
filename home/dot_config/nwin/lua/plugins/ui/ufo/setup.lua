534c8c1a21f570f199d61b470597e19d5d086926
krikchaip <krikchai.p@gmail.com>
2024-07-14 23:15:49 +0700

    nwin: ufo use treesitter provider

--- home/dot_config/nwin/lua/plugins/ui/ufo/setup.lua
+++ home/dot_config/nwin/lua/plugins/ui/ufo/setup.lua
@@ -2,4 +2,11 @@ local utils = require 'plugins.ui.ufo.utils'
 
 require('ufo').setup {
   fold_virt_text_handler = utils.folded_number_suffix,
+
+  -- use treesitter as a main provider
+  -- (Note: the `nvim-treesitter` plugin is *not* needed.)
+  -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+  provider_selector = function()
+    return { 'treesitter', 'indent' }
+  end,
 }
