ef74d45d13a12eca4e243fa9d030ae778c74cb7d
krikchaip <krikchai.p@gmail.com>
2024-07-20 02:42:31 +0700

    kitty-scrollback: tokyonight colorscheme

---
+++ home/dot_config/kitty-scrollback/lua/plugins/colorscheme/tokyonight.lua
@@ -0,0 +1,36 @@
+return {
+  -- Spec Source
+  'folke/tokyonight.nvim',
+  name = 'tokyonight',
+
+  -- Spec Loading
+  priority = 1000,
+
+  -- Spec Setup
+  config = function()
+    require('tokyonight').setup {
+      -- The theme comes in four styles, `moon`, `storm`, a darker variant `night` and `day`
+      style = 'storm',
+
+      -- Enable this to disable setting the background color
+      transparent = false,
+
+      styles = {
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        floats = 'transparent',
+      },
+
+      -- add any plugins here that you want to enable
+      -- for all possible plugins, see: https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+      plugins = {
+        -- enable all plugins highlights by default
+        all = true,
+      },
+    }
+
+    vim.cmd [[colorscheme tokyonight]]
+  end,
+
+  -- Spec Lazy Loading
+  lazy = false,
+}
