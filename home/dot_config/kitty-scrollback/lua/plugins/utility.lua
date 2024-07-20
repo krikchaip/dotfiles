5f071bfb7bd770058999a4322db2317601a760f5
krikchaip <krikchai.p@gmail.com>
2024-07-21 02:45:59 +0700

    kitty-scrollback: utility plugins

---
+++ home/dot_config/kitty-scrollback/lua/plugins/utility.lua
@@ -0,0 +1,7 @@
+return {
+  -- Useful for getting pretty icons, but requires a Nerd Font to be installed
+  { 'nvim-tree/nvim-web-devicons' },
+
+  -- UI Component Library for Neovim
+  { 'MunifTanjim/nui.nvim' },
+}
