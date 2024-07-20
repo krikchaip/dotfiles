c6b5803763b781e0883423339aa6505c25485142
krikchaip <krikchai.p@gmail.com>
2024-07-21 03:11:14 +0700

    kitty-scrollback: set inactive lualine sections

--- home/dot_config/kitty-scrollback/lua/plugins/ui/lualine/setup.lua
+++ home/dot_config/kitty-scrollback/lua/plugins/ui/lualine/setup.lua
@@ -3,33 +3,13 @@ local components = require 'plugins.ui.lualine.components'
 require('lualine').setup {
   options = {
     -- set `true` to have a single statusline at bottom of instead of one for every window
-    globalstatus = true,
+    globalstatus = false,
 
     -- sets how often lualine should refresh it's contents (in ms)
     refresh = {
       statusline = 250,
       tabline = 250,
       winbar = 250,
-    },
-
-    disabled_filetypes = {
-      winbar = {
-        'DiffviewFileHistory',
-        'DiffviewFiles',
-        'NvimTree',
-        'dashboard',
-        'noice',
-        'trouble',
-      },
-
-      statusline = {
-        'DiffviewFileHistory',
-        'DiffviewFiles',
-        'NvimTree',
-        'dashboard',
-        'noice',
-        'trouble',
-      },
     },
 
     -- which filetypes to always be drawn as inactive statusline
@@ -51,5 +31,12 @@ require('lualine').setup {
     lualine_y = { components.filetype },
     lualine_z = { 'searchcount', 'selectioncount', 'location' },
   },
-  inactive_sections = {},
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = { components.filetype },
+    lualine_z = { 'location' },
+  },
 }
