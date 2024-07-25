eba9ffaa2588b55d2573ec6d1aed44b76ddea26b
krikchaip <krikchai.p@gmail.com>
2024-07-26 02:32:39 +0700

    nvim: trouble turn off autocmd for quickfix list ui

--- home/dot_config/nvim/lua/plugins/ui/trouble/autocmds.lua
+++ home/dot_config/nvim/lua/plugins/ui/trouble/autocmds.lua
@@ -1,14 +1,12 @@
-local utils = require 'plugins.ui.trouble.utils'
-
 -- Automatically Open Trouble Quickfix
 -- ref: https://github.com/folke/trouble.nvim/blob/main/docs/examples.md#automatically-open-trouble-quickfix
-vim.api.nvim_create_autocmd('QuickFixCmdPost', {
-  desc = 'Automatically Open Trouble Quickfix',
-  group = vim.api.nvim_create_augroup('trouble-qflist-postcmd', { clear = true }),
-  callback = function()
-    vim.schedule(utils.handle_quickfix_open)
-  end,
-})
+-- vim.api.nvim_create_autocmd('QuickFixCmdPost', {
+--   desc = 'Automatically Open Trouble Quickfix',
+--   group = vim.api.nvim_create_augroup('trouble-qflist-postcmd', { clear = true }),
+--   callback = function()
+--     vim.schedule(require('plugins.ui.trouble.utils').handle_quickfix_open)
+--   end,
+-- })
 
 -- Open Trouble Quickfix when the qf list opens
 -- ref: https://github.com/folke/trouble.nvim/blob/main/docs/examples.md#open-trouble-quickfix-when-the-qf-list-opens
@@ -16,6 +14,6 @@ vim.api.nvim_create_autocmd('QuickFixCmdPost', {
 --   desc = 'Open Trouble Quickfix when the quickfix list opens',
 --   group = vim.api.nvim_create_augroup('trouble-qflist-trigger', { clear = true }),
 --   callback = function(ev)
---     if vim.bo[ev.buf].buftype == 'quickfix' then vim.schedule(utils.handle_quickfix_open) end
+--     if vim.bo[ev.buf].buftype == 'quickfix' then vim.schedule(require('plugins.ui.trouble.utils').handle_quickfix_open) end
 --   end,
 -- })
