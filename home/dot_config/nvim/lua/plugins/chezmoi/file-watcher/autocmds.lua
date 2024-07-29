d12cc54b0c00fcaa30b8b1ebadea58b2c639e01e
krikchaip <krikchai.p@gmail.com>
2024-07-30 04:37:00 +0700

    nvim: 🧹

--- home/dot_config/nvim/lua/plugins/chezmoi/file-watcher/autocmds.lua
+++ home/dot_config/nvim/lua/plugins/chezmoi/file-watcher/autocmds.lua
@@ -4,7 +4,6 @@ vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
   group = vim.api.nvim_create_augroup('chezmoi-watch', { clear = true }),
   pattern = { '*/.local/share/chezmoi/*' },
   callback = function()
-    -- vim.schedule(require('chezmoi.commands.__edit').watch)
     vim.schedule(require('plugins.chezmoi.file-watcher.utils').edit_and_apply)
   end,
 })
