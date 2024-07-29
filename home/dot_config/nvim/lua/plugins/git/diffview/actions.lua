c6dead1894a6f09f85a151d87f3d4fd557df6bba
krikchaip <krikchai.p@gmail.com>
2024-07-30 04:35:33 +0700

    nvim: diffview action refactor `close_diffview`

--- home/dot_config/nvim/lua/plugins/git/diffview/actions.lua
+++ home/dot_config/nvim/lua/plugins/git/diffview/actions.lua
@@ -1,8 +1,7 @@
 local M = require 'diffview.actions'
 
 M.close_diffview = function()
-  local ok, _ = pcall(vim.cmd, 'tabnext# | tabclose#')
-  if not ok then vim.cmd [[silent tabclose]] end
+  vim.cmd [[silent! tabnext# | silent! tabclose#]]
 end
 
 return M
