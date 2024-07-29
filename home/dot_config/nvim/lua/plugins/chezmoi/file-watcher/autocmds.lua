11e793d150d35b2cf7147539f3746136ac780fd6
krikchaip <krikchai.p@gmail.com>
2024-07-30 02:57:38 +0700

    nvim: revert chezmoi autocmd for now

--- home/dot_config/nvim/lua/plugins/chezmoi/file-watcher/autocmds.lua
+++ home/dot_config/nvim/lua/plugins/chezmoi/file-watcher/autocmds.lua
@@ -1,11 +1,10 @@
-local utils = require 'plugins.chezmoi.file-watcher.utils'
-
 -- Automatically apply changes on files under chezmoi source path
 vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
   desc = 'Automatically apply changes on files under chezmoi source path',
   group = vim.api.nvim_create_augroup('chezmoi-watch', { clear = true }),
   pattern = { '*/.local/share/chezmoi/*' },
   callback = function()
-    vim.schedule(utils.edit_and_apply)
+    -- vim.schedule(require('plugins.chezmoi.file-watcher.utils').edit_and_apply)
+    vim.schedule(require('chezmoi.commands.__edit').watch)
   end,
 })
