e6a562c42e8b262abca542a206f7d2ef36b73a5a
krikchaip <krikchai.p@gmail.com>
2024-07-30 03:52:59 +0700

    nvim: goto-preview remap split keys

--- home/dot_config/nvim/lua/plugins/lsp/goto-preview/keymaps.lua
+++ home/dot_config/nvim/lua/plugins/lsp/goto-preview/keymaps.lua
@@ -4,9 +4,9 @@ return {
       { 'q', actions.close_current, 'Preview: Close Current Window' },
       { 'Q', actions.close_all_wins, 'Preview: Close All Windows' },
       { '<CR>', actions.open_preview 'default', 'Preview: Replace Parent Window' },
-      { '<C-s>', actions.open_preview 'horizontal', 'Preview: Split Horizontally' },
-      { '<C-v>', actions.open_preview 'vertical', 'Preview: Split Vertically' },
-      { '<C-t>', actions.open_preview 'tab', 'Preview: Open in New Tab' },
+      { 's', actions.open_preview 'horizontal', 'Preview: Split Horizontally' },
+      { 'v', actions.open_preview 'vertical', 'Preview: Split Vertically' },
+      { 't', actions.open_preview 'tab', 'Preview: Open in New Tab' },
     }
   end,
 }
