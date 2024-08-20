42ed5e859104426d85f018d000d3fbb5d9900f6d
krikchaip <krikchai.p@gmail.com>
2024-08-21 04:23:25 +0700

    nvim: lualine truc util refactor

--- home/dot_config/nvim/lua/plugins/ui/lualine/utils.lua
+++ home/dot_config/nvim/lua/plugins/ui/lualine/utils.lua
@@ -1,17 +1,29 @@
 local M = {}
 
---- @param trunc_width number trunctates component when screen width is less then trunc_width
---- @param trunc_len number truncates component to trunc_len number of chars
---- @param hide_width number hides component when window width is smaller then hide_width
---- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
---- return function that can format the component accordingly
-function M.trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
-  return function(str)
-    local win_width = vim.fn.winwidth(0)
+--- @class TruncateOptions
+--- @field trunc_width? number truncates component when screen width is less then trunc_width
+--- @field trunc_len? number truncates component to trunc_len number of chars
+--- @field hide_width? number hides component when window width is smaller then hide_width
+--- @field no_ellipsis? boolean whether to disable adding '...' at end after truncation
+--- @field screen? boolean uses screen width instead of window width
 
-    if hide_width and win_width < hide_width then
+--- @param opts? TruncateOptions
+--- @return function fmt a function that can format the component accordingly
+function M.trunc(opts)
+  opts = opts or {}
+
+  local trunc_width = opts.trunc_width
+  local trunc_len = opts.trunc_len
+  local hide_width = opts.hide_width
+  local no_ellipsis = opts.no_ellipsis
+  local screen = opts.screen or false
+
+  return function(str)
+    local width = screen and vim.o.columns or vim.fn.winwidth(0)
+
+    if hide_width and width < hide_width then
       return ''
-    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+    elseif trunc_width and trunc_len and width < trunc_width and #str > trunc_len then
       return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
     end
 
