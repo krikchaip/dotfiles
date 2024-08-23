766859ab59a8b89066777ab4e3cb1b7fa90343bc
krikchaip <krikchai.p@gmail.com>
2024-08-24 01:42:09 +0700

    nvim: incline styling 

--- home/dot_config/nvim/lua/plugins/ui/incline/utils.lua
+++ home/dot_config/nvim/lua/plugins/ui/incline/utils.lua
@@ -1,15 +1,16 @@
 local devicons = require 'nvim-web-devicons'
 
-local LEFT_SEP = ' '
-local RIGHT_SEP = ' '
+local LUALINE_THEME = 'lualine.themes.auto'
+local LEFT_SEP = '◖'
+local RIGHT_SEP = '◗'
 
 local M = {}
 
-local function separator(symbol, props)
-  return { symbol }
+local function separator(symbol, theme)
+  return vim.tbl_extend('force', { symbol }, theme)
 end
 
-local function file_diagnostics(props)
+local function file_diagnostics(props, theme)
   local render = {}
 
   for severity, icon in pairs(vim.g.diagnostic_signs) do
@@ -19,7 +20,7 @@ local function file_diagnostics(props)
 
   if #render > 0 then table.insert(render, { '|', ' ' }) end
 
-  return render
+  return vim.tbl_extend('force', render, theme)
 end
 
 local function filename(props)
@@ -33,7 +34,7 @@ local function filename(props)
   return name
 end
 
-local function filename_with_icons(props)
+local function filename_with_icons(props, theme)
   local file_label = filename(props)
 
   local ft_icon, ft_color = devicons.get_icon_color(file_label)
@@ -42,15 +43,15 @@ local function filename_with_icons(props)
   local modified = vim.bo[props.buf].modified
   local modified_icon = modified and { ' ', '●' } or ''
 
-  return {
+  return vim.tbl_extend('force', {
     file_icon,
     file_label,
     modified_icon,
-  }
+  }, theme)
 end
 
 local function get_theme(props)
-  local ok, lualine_theme = pcall(require, 'lualine.themes.auto')
+  local ok, lualine_theme = pcall(require, LUALINE_THEME)
   if not ok then return { guifg = '#eeeeee', guibg = '#444444' } end
 
   local lualine_utils = require 'plugins.ui.lualine.utils'
@@ -65,13 +66,33 @@ local function get_theme(props)
   return { group = 'lualine_b_inactive' }
 end
 
+local function get_theme_sep(props)
+  local ok, lualine_theme = pcall(require, LUALINE_THEME)
+  if not ok then return { guifg = '#444444' } end
+
+  local lualine_utils = require 'plugins.ui.lualine.utils'
+  local highlight = require 'lualine.highlight'
+
+  if props.focused then
+    local mode = lualine_utils.get_mode()
+    local hl = lualine_theme[mode].b
+
+    return { guifg = hl.bg }
+  end
+
+  return { guifg = highlight.get_lualine_hl('lualine_b_inactive').bg }
+end
+
 M.render = function(props)
-  return vim.tbl_extend('force', {
-    separator(LEFT_SEP, props),
-    file_diagnostics(props),
-    filename_with_icons(props),
-    separator(RIGHT_SEP, props),
-  }, get_theme(props))
+  local theme = get_theme(props)
+  local theme_sep = get_theme_sep(props)
+
+  return {
+    separator(LEFT_SEP, theme_sep),
+    file_diagnostics(props, theme),
+    filename_with_icons(props, theme),
+    separator(RIGHT_SEP, theme_sep),
+  }
 end
 
 return M
