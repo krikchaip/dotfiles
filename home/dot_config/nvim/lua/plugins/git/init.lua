776934b7a78c1e8cd27353c8d63ed9e302eef57a
krikchaip <krikchai.p@gmail.com>
2024-11-06 04:55:10 +0700

    nvim: git disable diffview & neogit in favor of lazygit

--- home/dot_config/nvim/lua/plugins/git/init.lua
+++ home/dot_config/nvim/lua/plugins/git/init.lua
@@ -1,5 +1,6 @@
 return {
-  require 'plugins.git.diffview',
   require 'plugins.git.gitsigns',
-  require 'plugins.git.neogit',
+
+  -- require 'plugins.git.diffview',
+  -- require 'plugins.git.neogit',
 }
