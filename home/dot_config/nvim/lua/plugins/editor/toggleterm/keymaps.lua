42d255e400e4bfe72c7dfb0c61a1b7520d4e6271
krikchaip <krikchai.p@gmail.com>
2024-11-06 17:07:12 +0700

    nvim: toggleterm lazygit keys rewrite desc

--- home/dot_config/nvim/lua/plugins/editor/toggleterm/keymaps.lua
+++ home/dot_config/nvim/lua/plugins/editor/toggleterm/keymaps.lua
@@ -4,8 +4,8 @@ local lazygit = integration.lazygit()
 
 vim.keymap.set('n', '<C-g>', function()
   lazygit.default:toggle()
-end, { desc = 'Lazygit: Toggle Source Control' })
+end, { desc = 'Git: Toggle Lazygit 💤' })
 
 vim.keymap.set('n', '<leader>gb', function()
   lazygit.branch:toggle()
-end, { desc = 'Lazygit: Manage Branches' })
+end, { desc = 'Git: Manage Branches' })
