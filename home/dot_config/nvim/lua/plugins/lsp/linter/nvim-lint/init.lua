f61a8748c9cda4c0f079a946e3fcd58ea8e9d4ac
krikchaip <krikchai.p@gmail.com>
2024-11-27 16:32:38 +0700

    nvim: fix eslint_d error when no config file presents

--- home/dot_config/nvim/lua/plugins/lsp/linter/nvim-lint/init.lua
+++ home/dot_config/nvim/lua/plugins/lsp/linter/nvim-lint/init.lua
@@ -6,6 +6,7 @@ return {
   -- Spec Setup
   config = function()
     local lint = require 'lint'
+    local util = require 'lint.util'
 
     lint.linters_by_ft = {
       javascript = { 'eslint_d' },
@@ -15,6 +16,18 @@ return {
       elixir = { 'credo' },
       go = { 'golangcilint' },
     }
+
+    -- fix eslint_d warning when no config file presents
+    -- ref: https://github.com/mfussenegger/nvim-lint/issues/462
+    lint.linters.eslint_d = util.wrap(lint.linters.eslint_d, function(diagnostic)
+      -- try to ignore "No ESLint configuration found" error
+      -- if diagnostic.message:find("Error: No ESLint configuration found") then -- old version
+      -- update: 20240814, following is working
+      ---@diagnostic disable-next-line: return-type-mismatch
+      if diagnostic.message:find 'Error: Could not find config file' then return nil end
+
+      return diagnostic
+    end)
 
     require 'plugins.lsp.linter.nvim-lint.autocmds'
   end,
