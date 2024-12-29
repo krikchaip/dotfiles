e130151efa35cd881cc113858e5c8fedf94a1433
krikchaip <krikchai.p@gmail.com>
2024-12-30 04:19:21 +0700

    nvim: go template syntax highlight for *.tmpl files

--- home/dot_config/nvim/lua/plugins/lsp/lang/go/autocmds.lua
+++ home/dot_config/nvim/lua/plugins/lsp/lang/go/autocmds.lua
@@ -2,7 +2,16 @@ vim.api.nvim_create_autocmd('FileType', {
   desc = 'Golang-specific buffer configurations',
   group = vim.api.nvim_create_augroup('lsp-lang-go', { clear = true }),
   pattern = { 'go' },
-  callback = function(opts)
+  callback = function(_)
     -- NOTE: use `opts.buf` for buffer specific options
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  desc = 'Go template syntax highlighting',
+  group = vim.api.nvim_create_augroup('lsp-lang-go-template', { clear = true }),
+  pattern = { '*.tmpl.*', '*.tmpl' },
+  callback = function()
+    vim.cmd [[set syntax=gotmpl]]
   end,
 })
