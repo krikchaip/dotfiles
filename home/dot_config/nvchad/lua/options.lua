29a829fecf8671231b3427a1f20cfd7c015397ea
krikchaip <krikchai.p@gmail.com>
2025-06-13 15:50:50 +0700

    nvchad: global options

--- home/dot_config/nvchad/lua/options.lua
+++ home/dot_config/nvchad/lua/options.lua
@@ -1,6 +1,38 @@
 require "nvchad.options"
 
--- add yours here!
+local opt = vim.opt
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- to enable cursorline!
+o.cursorlineopt = "both"
+
+-- do not wrap lines
+o.wrap = false
+
+-- every wrapped line will continue visually indented
+o.breakindent = true
+
+-- minimal number of screen lines to keep above and below the cursor.
+o.scrolloff = 5
+
+-- columns to scroll horizontally when texts are off the screen
+o.sidescrolloff = 4
+
+-- no swapfile. EVER 😠
+o.swapfile = false
+
+-- set highlight on search
+o.hlsearch = true
+
+-- preview substitutions live, as you type!
+o.inccommand = "split"
+
+-- prevent folds to accidentally open while moving horizontally
+opt.foldopen:remove "hor"
+
+-- sets how neovim will display certain whitespace characters in the editor.
+o.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- enable 24-bit colour in terminal
+o.termguicolors = true
