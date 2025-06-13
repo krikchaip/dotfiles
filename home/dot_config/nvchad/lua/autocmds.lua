1b6acdef18ca60d559f93603b9de02650232d475
krikchaip <krikchai.p@gmail.com>
2025-06-14 00:15:49 +0700

    nvchad: copied configs from recipes section

--- home/dot_config/nvchad/lua/autocmds.lua
+++ home/dot_config/nvchad/lua/autocmds.lua
@@ -1,1 +1,44 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- highlight text after yanking
+-- ref: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua#L196-L205
+autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- restore cursor position on file open
+-- ref: https://nvchad.com/docs/recipes
+autocmd("BufReadPost", {
+  desc = "Restore cursor position on file open",
+  group = augroup("restore-cursor", { clear = true }),
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
+-- show nvdash when all buffers are closed
+-- ref: https://nvchad.com/docs/recipes
+autocmd("BufDelete", {
+  desc = "Show NvDash when all buffers are closed",
+  group = augroup("show-nvdash", { clear = true }),
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then vim.cmd "Nvdash" end
+  end,
+})
--- home/dot_config/nvchad/lua/configs/lspconfig.lua
+++ home/dot_config/nvchad/lua/configs/lspconfig.lua
@@ -1,6 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+-- read :h vim.lsp.config for changing options of lsp servers
+local servers = {
+  html = {},
+  cssls = {},
+}
 
--- read :h vim.lsp.config for changing options of lsp servers 
+for name, opts in pairs(servers) do
+  vim.lsp.enable(name)
+  vim.lsp.config(name, opts)
+end
