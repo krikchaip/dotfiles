75605c8e167ebdad0b4f421890ec721746946d4b
krikchaip <krikchai.p@gmail.com>
2025-06-14 05:45:34 +0700

    nvchad: telescope scaffold

---
+++ home/dot_config/nvchad/lua/configs/telescope.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+M.config = function(opts) end
+
+return M
--- home/dot_config/nvchad/lua/plugins/init.lua
+++ home/dot_config/nvchad/lua/plugins/init.lua
@@ -13,16 +13,26 @@ return {
     end,
   },
 
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      require("configs.telescope").config(opts)
+    end,
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
   -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = {
+  --     ensure_installed = {
+  --       "vim",
+  --       "lua",
+  --       "vimdoc",
+  --       "html",
+  --       "css",
+  --     },
+  --   },
   -- },
 }
