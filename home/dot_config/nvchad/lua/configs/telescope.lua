b0da013cdf1dbf33aa3b07e5c53b54056ef21f3a
krikchaip <krikchai.p@gmail.com>
2025-06-14 16:51:02 +0700

    nvchad: default telescope picker keys

--- home/dot_config/nvchad/lua/configs/telescope.lua
+++ home/dot_config/nvchad/lua/configs/telescope.lua
@@ -1,5 +1,23 @@
 local M = {}
 
-M.config = function(opts) end
+M.config = function(opts)
+  opts.defaults.mappings = {
+    i = {
+      -- close prompt
+      ["<C-c>"] = false,
+      ["<ESC>"] = "close",
+
+      -- scrolling
+      ["<PageDown>"] = false,
+      ["<PageUp>"] = false,
+      ["<M-d>"] = "results_scrolling_down",
+      ["<M-u>"] = "results_scrolling_up",
+
+      -- item selection (qflist)
+      ["<C-q>"] = false,
+      ["<M-Tab>"] = "drop_all",
+    },
+  }
+end
 
 return M
