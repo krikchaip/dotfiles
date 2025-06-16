6c6570e5da23b7156d992be839e2e71d8147fede
krikchaip <krikchai.p@gmail.com>
2025-06-16 13:49:15 +0700

    nvchad: remove lsp default keys

--- home/dot_config/nvchad/lua/configs/lspconfig.lua
+++ home/dot_config/nvchad/lua/configs/lspconfig.lua
@@ -1,7 +1,16 @@
 local M = {}
 
+local autocmd = vim.api.nvim_create_autocmd
+local nomap = vim.keymap.del
+
 M.setup = function()
   require("nvchad.configs.lspconfig").defaults()
+
+  autocmd("LspAttach", {
+    callback = function(args)
+      M.on_attach(args.buf)
+    end,
+  })
 
   -- read :h vim.lsp.config for changing options of lsp servers
   local servers = {
@@ -32,6 +41,20 @@ M.setup = function()
     vim.lsp.config(name, opts)
     vim.lsp.enable(name)
   end
+end
+
+M.on_attach = function(buf)
+  local opts = { buffer = buf }
+
+  -- remove default nvchad mappings
+  -- ref: https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/configs/lspconfig.lua
+  nomap("n", "gD", opts)
+  nomap("n", "gd", opts)
+  nomap("n", "<leader>wa", opts)
+  nomap("n", "<leader>wr", opts)
+  nomap("n", "<leader>wl", opts)
+  nomap("n", "<leader>D", opts)
+  nomap("n", "<leader>ra", opts)
 end
 
 return M
