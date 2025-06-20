4f943578e8863835e414689398d8ea1e72168761
krikchaip <krikchai.p@gmail.com>
2025-06-20 14:16:59 +0700

    nvchad: fix possession hlsearch

--- home/dot_config/nvchad/lua/configs/possession.lua
+++ home/dot_config/nvchad/lua/configs/possession.lua
@@ -3,7 +3,17 @@ local M = {}
 M.config = function(opts)
   opts.prompt_no_cr = true
   opts.autosave = { current = true, cwd = true }
-  opts.plugins = { delete_buffers = true, delete_hidden_buffers = false }
+
+  opts.hooks = {
+    after_load = function()
+      vim.cmd "let @/=''"
+    end,
+  }
+
+  opts.plugins = {
+    delete_buffers = true,
+    delete_hidden_buffers = false,
+  }
 
   return opts
 end
