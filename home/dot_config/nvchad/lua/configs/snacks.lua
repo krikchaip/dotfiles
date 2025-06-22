765cedfa9a52e8bc6363ab69e405fad63b3455cf
krikchaip <krikchai.p@gmail.com>
2025-06-22 15:26:37 +0700

    nvchad: temp fix for golangci-lint code 5

--- home/dot_config/nvchad/lua/configs/snacks.lua
+++ home/dot_config/nvchad/lua/configs/snacks.lua
@@ -4,9 +4,17 @@ local M = {}
 ---@param opts snacks.Config
 M.config = function(opts)
   opts.image = { enabled = true }
-  opts.notifier = { enabled = true }
+  opts.notifier = { enabled = true, filter = M.notifier_filter }
 
   return opts
+end
+
+---@param notif snacks.notifier.Notif
+M.notifier_filter = function(notif)
+  -- temporary fix for https://github.com/mfussenegger/nvim-lint/issues/744
+  if notif.msg:find "`golangci%-lint` exited with code: 5" then return false end
+
+  return true
 end
 
 return M
