dca296684e8164e6edd52a13fdabffc14a16a2f1
krikchaip <krikchai.p@gmail.com>
2025-06-23 16:51:46 +0700

    nvchad: mini.files open + reset key remapped

--- home/dot_config/nvchad/lua/configs/mini/files.lua
+++ home/dot_config/nvchad/lua/configs/mini/files.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+local map = vim.keymap.set
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
@@ -32,11 +33,46 @@ M.setup = function(opts)
       Snacks.rename.on_rename_file(args.data.from, args.data.to)
     end,
   })
+
+  autocmd("User", {
+    group = augroup("mini-files.mappings", { clear = true }),
+    pattern = "MiniFilesBufferCreate",
+    callback = function(args)
+      M.on_attach(args.data.buf_id)
+    end,
+  })
 end
 
+M.on_attach = function(bufnr)
+  local function opts(desc)
+    return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  map("n", "<BS>", M.reset, opts "Reset")
+end
+
+-- open and select the current buffer in mini files
+-- ref: https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/mini-files.lua#L87-L100
 M.open = function()
   local MiniFiles = require "mini.files"
-  MiniFiles.open()
+
+  local buf_name = vim.api.nvim_buf_get_name(0)
+  local dir_name = vim.fn.fnamemodify(buf_name, ":p:h")
+
+  if vim.fn.filereadable(buf_name) == 1 then
+    MiniFiles.open(buf_name, false)
+    MiniFiles.reveal_cwd()
+  elseif vim.fn.isdirectory(dir_name) == 1 then
+    MiniFiles.open(dir_name, false)
+    MiniFiles.reveal_cwd()
+  else
+    MiniFiles.open(nil, false)
+  end
+end
+
+M.reset = function()
+  MiniFiles.reset()
+  MiniFiles.reveal_cwd()
 end
 
 return M
