9b97a45a42eca921f632cdbe08f00789eb39fa97
krikchaip <krikchai.p@gmail.com>
2025-07-17 21:35:20 +0700

    nvchad: adjust lil bit for better ux

--- home/dot_config/nvchad/lua/configs/tree.lua
+++ home/dot_config/nvchad/lua/configs/tree.lua
@@ -42,7 +42,7 @@ end
 M.setup = function(opts)
   require("nvim-tree").setup(M.config(opts))
 
-  vim.g.auto_reveal = false
+  vim.g.auto_reveal = true
 
   autocmd("BufEnter", {
     desc = "Auto reveal current buffer in Nvim-tree",
