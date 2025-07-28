dc589436c1bd155270349bb4d6ce4006b5caba8a
krikchaip <krikchai.p@gmail.com>
2025-07-28 13:37:48 +0700

    nvim-lint: fix pylint doesn't see venv packages

--- home/dot_config/nvchad/lua/configs/lint.lua
+++ home/dot_config/nvchad/lua/configs/lint.lua
@@ -26,6 +26,25 @@ M.setup = function()
     return diagnostic
   end)
 
+  -- fix pylint displays import errors (unable to find packages from .venv)
+  -- ref: https://github.com/mason-org/mason.nvim/issues/1336
+  --      https://vi.stackexchange.com/questions/45737/pylint-unable-to-find-imports-from-currently-active-virtual-environment
+  table.insert(linters.pylint.args, "--init-hook")
+  table.insert(
+    linters.pylint.args,
+    "import base64;"
+      .. "exec(base64.b64decode('"
+      .. "aW1wb3J0IHN5cywgb3MKCmlmICdWSVJUVUFMX0VOVicgbm90IGluIG9zLmVudmlyb246CiAgICBze"
+      .. "XMuZXhpdCgwKQoKdmVfZGlyID0gb3MuZW52aXJvblsnVklSVFVBTF9FTlYnXQppZiB2ZV9kaXIgbm9"
+      .. "0IGluIHN5cy5wYXRoOgogICAgc3lzLnBhdGguaW5zZXJ0KDAsIHZlX2RpcikKCmFjdGl2YXRlX3RoaXM"
+      .. "gPSBvcy5wYXRoLmpvaW4odmVfZGlyLCAnYmluJywgJ2FjdGl2YXRlX3RoaXMucHknKQoKIyBGaXgg"
+      .. "Zm9yIHdpbmRvd3MKaWYgbm90IG9zLnBhdGguZXhpc3RzKGFjdGl2YXRlX3RoaXMpOgogICAgYWN"
+      .. "0aXZhdGVfdGhpcyA9IG9zLnBhdGguam9pbih2ZV9kaXIsICdTY3JpcHRzJywgJ2FjdGl2YXRlX3RoaXM"
+      .. "ucHknKQoKd2l0aCBvcGVuKGFjdGl2YXRlX3RoaXMsICdyJykgYXMgZjoKICAgIGV4ZWMoZi5yZWFkK"
+      .. "CksIHsnX19maWxlX18nOiBhY3RpdmF0ZV90aGlzfSkK"
+      .. "'))"
+  )
+
   vim.cmd "au BufEnter,BufWritePost,InsertLeave * lua require('lint').try_lint()"
 end
 
