6893bd395112e8a5e3cc23fb454f423596fa4de1
krikchaip <krikchai.p@gmail.com>
2025-10-08 00:58:42 +0700

    nvchad: ImgClip configure global opts

--- home/dot_config/nvchad/lua/configs/img-clip.lua
+++ home/dot_config/nvchad/lua/configs/img-clip.lua
@@ -1,12 +1,14 @@
 local M = {}
 
 M.config = function(opts)
+  opts.default = {
+    dir_path = "/tmp/img-clip",
+    use_absolute_path = true,
+    prompt_for_file_name = false,
+  }
+
   opts.filetypes = {
-    codecompanion = {
-      prompt_for_file_name = false,
-      template = "[Image]($FILE_PATH)",
-      use_absolute_path = true,
-    },
+    codecompanion = { template = "[Image]($FILE_PATH)" },
   }
 
   return opts
--- home/dot_config/nvchad/lua/mappings.lua
+++ home/dot_config/nvchad/lua/mappings.lua
@@ -254,5 +254,5 @@ map("n", "<M-a>", LLM.ToggleChat, { desc = "LLM: Toggle Chat" })
 map("v", "<M-a>", LLM.Inline, { desc = "LLM: Inline Visual", expr = true })
 map({ "n", "v" }, "<leader>a", LLM.Actions, { desc = "LLM: Show Actions" })
 
--- image content
-map("n", "<leader>p", "<cmd>PasteImage<CR>", { desc = "ImgClip: Paste Image from Clipboard" })
+-- paste content
+map("n", "<leader>pi", "<cmd>PasteImage<CR>", { desc = "Paste: Image From Clipboard" })
