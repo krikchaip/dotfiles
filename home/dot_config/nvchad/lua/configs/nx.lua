167f8f4423b5e33ac42b6c617499dcf06cac12e5
krikchaip <krikchai.p@gmail.com>
2025-12-08 16:55:04 +0700

    nx.nvim: prefix 'nx' command with corresponding package manager

--- home/dot_config/nvchad/lua/configs/nx.lua
+++ home/dot_config/nvchad/lua/configs/nx.lua
@@ -8,11 +8,32 @@ M.condition = function()
 end
 
 M.config = function(opts)
+  opts.nx_cmd_root = M.prefix_package_manager "nx"
+
   return opts
 end
 
 M.setup = function(opts)
   require("nx").setup(M.config(opts))
+end
+
+---@param command string
+M.prefix_package_manager = function(command)
+  local cwd = vim.uv.cwd()
+
+  -- order lockfiles by priority
+  local lockfiles = {
+    { name = "bun.lock", cmd = "bun" },
+    { name = "pnpm-lock.yaml", cmd = "pnpm" },
+    { name = "yarn.lock", cmd = "yarn" },
+  }
+
+  local prefix = vim.iter(lockfiles):find(function(lockfile)
+    local lockfile_path = vim.fs.joinpath(cwd, lockfile.name)
+    return vim.fn.filereadable(lockfile_path) == 1
+  end) or { cmd = "npm" }
+
+  return string.format("%s %s", prefix.cmd, command)
 end
 
 return M
