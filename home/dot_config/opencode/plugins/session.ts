806e66416f60ed8a0b31b9a8955c5681e9f5e9ea
krikchaip <krikchai.p@gmail.com>
2026-05-02 05:40:24 +0700

    opencode(plugin): session walk tool

--- home/dot_config/opencode/plugins/session.ts
+++ home/dot_config/opencode/plugins/session.ts
@@ -1,13 +1,36 @@
 import { type Plugin, tool } from "@opencode-ai/plugin";
 
-export const SessionPlugin: Plugin = async () => {
+export const SessionPlugin: Plugin = async ({ client }) => {
   return {
     tool: {
       session_me: tool({
         description: "Get the current session ID",
         args: {},
-        async execute(args, context) {
+        async execute(_, context) {
           return context.sessionID;
+        },
+      }),
+      session_walk: tool({
+        description:
+          "Find the root ancestor session ID for a given session. If not provided, it defaults to the current session ID",
+        args: {
+          sessionID: tool.schema
+            .string()
+            .optional()
+            .describe("The starting session ID"),
+        },
+        async execute(args, context) {
+          let currentId = args.sessionID || context.sessionID;
+
+          while (true) {
+            const res = await client.session.get({ path: { id: currentId } });
+
+            if (!res.data?.parentID) {
+              return currentId;
+            }
+
+            currentId = res.data.parentID;
+          }
         },
       }),
     },
